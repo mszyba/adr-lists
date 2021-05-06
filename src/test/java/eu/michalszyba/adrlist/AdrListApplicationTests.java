d7942cd8a84d5811b9e2497d928fdfa42e8bbcf0
Michał Szyba <m.szyba@gmail.com>
2021-05-06 14:34:06 +0200

    AdrListApplication.java

---
+++ src/test/java/eu/michalszyba/adrlist/AdrListApplicationTests.java
@@ -0,0 +1,13 @@
+package eu.michalszyba.adrlist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdrListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
