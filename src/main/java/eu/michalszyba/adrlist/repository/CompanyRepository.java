c168e3cce62839e6a5fe59e13b9cb8b7f2f2769c
Michał Szyba <m.szyba@gmail.com>
2021-05-07 08:47:04 +0200

    updated interface CompanyRepository

--- src/main/java/eu/michalszyba/adrlist/repository/CompanyRepository.java
+++ src/main/java/eu/michalszyba/adrlist/repository/CompanyRepository.java
@@ -4,10 +4,6 @@ import eu.michalszyba.adrlist.entity.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
 public interface CompanyRepository extends JpaRepository<Company, Long> {
-
-    List<Company> findByCompanyName(String companyName);
 }
