939ceb6e79b8378bcd91ddbeb22afe230c75692b
Michał Szyba <m.szyba@gmail.com>
2021-05-08 11:25:30 +0200

    added UserController (post add, get add, get list)

---
+++ src/main/java/eu/michalszyba/adrlist/controller/UserController.java
@@ -0,0 +1,52 @@
+package eu.michalszyba.adrlist.controller;
+
+import eu.michalszyba.adrlist.model.User;
+import eu.michalszyba.adrlist.repository.CompanyRepository;
+import eu.michalszyba.adrlist.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserRepository userRepository;
+    private final CompanyRepository companyRepository;
+
+    @Autowired
+    public UserController(UserRepository userRepository, CompanyRepository companyRepository) {
+        this.userRepository = userRepository;
+        this.companyRepository = companyRepository;
+    }
+
+    @GetMapping("/list")
+    public String listUser(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "/user/listUser";
+    }
+
+    @GetMapping("/add")
+    public String getAddUserForm(Model model) {
+        model.addAttribute("user", new User());
+        model.addAttribute("companies", companyRepository.findAll());
+        return "/user/addUser";
+    }
+
+    @PostMapping("/add")
+    public String postAddUserForm(@ModelAttribute User user, Model model) {
+        model.addAttribute("user", user);
+        userRepository.save(user);
+        return "redirect:/user/list";
+    }
+
+
+
+
+
+
+}
