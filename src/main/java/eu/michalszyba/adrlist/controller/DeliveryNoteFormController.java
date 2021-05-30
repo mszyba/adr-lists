0f9e4bda90e0ae7bb54af3d8da5e9ef7d4e2b1fd
Michał Szyba <m.szyba@gmail.com>
2021-05-30 22:57:40 +0200

    added Packaging

--- src/main/java/eu/michalszyba/adrlist/controller/DeliveryNoteFormController.java
+++ src/main/java/eu/michalszyba/adrlist/controller/DeliveryNoteFormController.java
@@ -4,14 +4,8 @@ import com.fasterxml.jackson.core.JsonProcessingException;
 import eu.michalszyba.adrlist.converter.JsonConverter;
 import eu.michalszyba.adrlist.form.DeliveryNoteForm;
 import eu.michalszyba.adrlist.form.UnForm;
-import eu.michalszyba.adrlist.model.DeliveryNote;
-import eu.michalszyba.adrlist.model.Company;
-import eu.michalszyba.adrlist.model.Customer;
-import eu.michalszyba.adrlist.model.Un;
-import eu.michalszyba.adrlist.service.DeliveryNoteService;
-import eu.michalszyba.adrlist.service.CompanyService;
-import eu.michalszyba.adrlist.service.CustomerService;
-import eu.michalszyba.adrlist.service.UnService;
+import eu.michalszyba.adrlist.model.*;
+import eu.michalszyba.adrlist.service.*;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -26,15 +20,17 @@ public class DeliveryNoteFormController {
     private final CustomerService customerService;
     private final UnService unService;
     private final DeliveryNoteService deliveryNoteService;
+    private final PackagingService packagingService;
 
     public DeliveryNoteFormController(CompanyService companyService,
                                       CustomerService customerService,
                                       UnService unService,
-                                      DeliveryNoteService deliveryNoteService) {
+                                      DeliveryNoteService deliveryNoteService, PackagingService packagingService) {
         this.companyService = companyService;
         this.customerService = customerService;
         this.unService = unService;
         this.deliveryNoteService = deliveryNoteService;
+        this.packagingService = packagingService;
     }
 
     @ModelAttribute("unList")
@@ -52,6 +48,11 @@ public class DeliveryNoteFormController {
         return customerService.getAllCustomer();
     }
 
+    @ModelAttribute("packagingList")
+    public List<Packaging> populatePackaging() {
+        return packagingService.getAllPackages();
+    }
+
     @GetMapping("/add")
     public String getForm(Model model) {
         model.addAttribute("deliveryNoteForm", new DeliveryNoteForm());
@@ -65,15 +66,12 @@ public class DeliveryNoteFormController {
     }
 
     @PostMapping(value = "/add", params = {"saveForm"})
-    public String postFirstStepForm(DeliveryNoteForm deliveryNoteForm, Model model) throws JsonProcessingException {
-        System.out.println("===========" + deliveryNoteForm);
-
+    public String postFirstStepForm(DeliveryNoteForm deliveryNoteForm, Model model) {
         DeliveryNote deliveryNote = new DeliveryNote();
 
         Long company_id = deliveryNoteForm.getCompany().getId();
         Long customer_id = deliveryNoteForm.getCustomer().getId();
         String columnDeliveryNoteForm = new JsonConverter().convertToDatabaseColumn(deliveryNoteForm);
-
 
         deliveryNote.setCompanyId(company_id);
         deliveryNote.setCustomerId(customer_id);
