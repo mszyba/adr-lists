e322324d5a6674a50bc6cb02164bd0c77ae8e892
Michał Szyba <m.szyba@gmail.com>
2021-06-05 00:35:28 +0200

    added UserRole and additional needed

--- src/main/java/eu/michalszyba/adrlist/security/SecurityConfig.java
+++ src/main/java/eu/michalszyba/adrlist/security/SecurityConfig.java
@@ -1,10 +1,12 @@
 package eu.michalszyba.adrlist.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -14,7 +16,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
             .authorizeRequests()
-                .antMatchers("/webjars/**", "/css/**").permitAll()
+                .antMatchers("/webjars/**", "/css/**", "/register").permitAll()
                 .anyRequest().authenticated()
             .and()
             .formLogin()
@@ -23,6 +25,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll()
                 .and()
             .logout()
+                .logoutUrl("/logout")
+                .logoutSuccessUrl("/login")
                 .permitAll();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
     }
 }
---
+++ src/main/java/eu/michalszyba/adrlist/service/UserRoleService.java
@@ -0,0 +1,8 @@
+package eu.michalszyba.adrlist.service;
+
+import eu.michalszyba.adrlist.model.UserRole;
+
+public interface UserRoleService {
+
+    UserRole getUserRoleByRole(String role);
+}
