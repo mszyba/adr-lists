ed048411d395a844bf4fef7e00f1156e54a37747
Michał Szyba <m.szyba@gmail.com>
2021-07-18 11:31:10 +0200

    deleted UnServiceImpl.java

--- src/main/java/eu/michalszyba/adrlist/service/UnService.java
+++ src/main/java/eu/michalszyba/adrlist/service/UnService.java
@@ -1,10 +1,21 @@
 package eu.michalszyba.adrlist.service;
 
 import eu.michalszyba.adrlist.model.Un;
+import eu.michalszyba.adrlist.repository.UnRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface UnService {
+@Service
+public class UnService {
 
-    List<Un> getAllUn();
+    private final UnRepository unRepository;
+
+    public UnService(UnRepository unRepository) {
+        this.unRepository = unRepository;
+    }
+
+    public List<Un> getAllUn() {
+        return unRepository.findAll();
+    }
 }
