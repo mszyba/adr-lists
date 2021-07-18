90672d0d37fdd24cfa4b081ccc0f0fa47e414985
Michał Szyba <m.szyba@gmail.com>
2021-07-18 11:32:14 +0200

    deleted PackagingServiceImpl.java

--- src/main/java/eu/michalszyba/adrlist/service/PackagingService.java
+++ src/main/java/eu/michalszyba/adrlist/service/PackagingService.java
@@ -1,10 +1,21 @@
 package eu.michalszyba.adrlist.service;
 
 import eu.michalszyba.adrlist.model.Packaging;
+import eu.michalszyba.adrlist.repository.PackagingRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-public interface PackagingService {
+@Service
+public class PackagingService {
 
-    List<Packaging> getAllPackages();
+    private final PackagingRepository packagingRepository;
+
+    public PackagingService(PackagingRepository packagingRepository) {
+        this.packagingRepository = packagingRepository;
+    }
+
+    public List<Packaging> getAllPackages() {
+        return packagingRepository.findAll();
+    }
 }
