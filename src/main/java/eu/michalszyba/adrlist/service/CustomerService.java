81981ac9fdf9f3618908e3809758f87a95dad8df
Michał Szyba <m.szyba@gmail.com>
2021-07-18 11:29:35 +0200

    deleted CustomerServiceImp.java

--- src/main/java/eu/michalszyba/adrlist/service/CustomerService.java
+++ src/main/java/eu/michalszyba/adrlist/service/CustomerService.java
@@ -1,20 +1,48 @@
 package eu.michalszyba.adrlist.service;
 
 import eu.michalszyba.adrlist.model.Customer;
+import eu.michalszyba.adrlist.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
-public interface CustomerService {
+@Service
+public class CustomerService {
 
-    List<Customer> getAllCustomer();
+    private final CustomerRepository customerRepository;
 
-    void saveCustomer(Customer customer);
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
-    void deleteCustomerById(Long id);
+    public List<Customer> getAllCustomer() {
+        return customerRepository.findAll();
+    }
 
-    Customer getCustomerById(Long id);
+    public void saveCustomer(Customer customer) {
+        this.customerRepository.save(customer);
+    }
 
-    List<Customer> getAllCustomerByCompanyId(Long companyId);
+    public void deleteCustomerById(Long id) {
+        this.customerRepository.deleteById(id);
+    }
 
-    List<Customer> getAllCustomerByUserId(Long userId);
+    public Customer getCustomerById(Long id) {
+        Optional<Customer> optionalCustomer = customerRepository.findById(id);
+        if (optionalCustomer.isPresent()) {
+            return optionalCustomer.get();
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    public List<Customer> getAllCustomerByCompanyId(Long companyId) {
+        return customerRepository.findAllByCompanyId(companyId);
+    }
+
+    public List<Customer> getAllCustomerByUserId(Long userId) {
+        return null;
+    }
 }
