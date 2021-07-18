ef5bfa894c4032e00fa89c0624990cb7f44bc7fc
Michał Szyba <m.szyba@gmail.com>
2021-07-18 11:34:05 +0200

    deleted UserServiceImpl.java

--- src/main/java/eu/michalszyba/adrlist/service/UserService.java
+++ src/main/java/eu/michalszyba/adrlist/service/UserService.java
@@ -1,20 +1,56 @@
 package eu.michalszyba.adrlist.service;
 
 import eu.michalszyba.adrlist.model.User;
+import eu.michalszyba.adrlist.repository.UserRepository;
+import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
+import java.util.Optional;
 
-public interface UserService {
+@Service
+public class UserService {
 
-    List<User> getAllUser();
+    private final UserRepository userRepository;
 
-    void saveUser(User user);
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
-    void updateUser(User user);
+    public List<User> getAllUser() {
+        return userRepository.findAll();
+    }
 
-    void softDeleteUserById(Long id);
+    public void saveUser(User user) {
+        this.userRepository.save(user);
+    }
 
-    User getUserById(Long id);
+    public void updateUser(User user) {
+        Long userId = user.getId();
+        String firstName = user.getFirstName();
+        String lastName = user.getLastName();
+        String password = user.getPassword();
+        String description = user.getDescription();
 
-    List<User> getAllUserByCompanyId(Long companyId);
+        this.userRepository.updateUser(firstName, lastName, password, description, userId);
+    }
+
+    public void softDeleteUserById(Long id) {
+        this.userRepository.softDelete(id);
+    }
+
+    public User getUserById(Long id) {
+        Optional<User> optionalUser = userRepository.findById(id);
+        if (optionalUser.isPresent()) {
+            User user = optionalUser.get();
+            user.setPassword("");
+            return user;
+        } else {
+            throw new EntityNotFoundException();
+        }
+    }
+
+    public List<User> getAllUserByCompanyId(Long companyId) {
+        return userRepository.findAllByCompanyId(companyId);
+    }
 }
